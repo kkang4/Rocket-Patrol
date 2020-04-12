4a1b63510367a0a6b8949823eeb65479fdd68730
Sean <kkang4@ucsc.edu>
2020-04-11 23:09:41 -0700

    Rocket Patrollll

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -1,17 +1,21 @@
-// Spaceship prefab
 class Spaceship extends Phaser.GameObjects.Sprite {
-    constructor(scene, x, y, texture, frame, pointValue) {
+    constructor(scene, x, y, texture, frame, pointValue){
         super(scene, x, y, texture, frame);
         scene.add.existing(this);
+        //store pointValue
         this.points = pointValue;
     }
 
     update(){
         //move spaceship left
-        this.x -= 3;
-        //wraparound screen bounds
-        if(this.x <= 0-this.width) {
-            this.x = game.config.width;
+        this.x -= game.settings.spaceshipSpeed;
+        //wraparound from left to right edge
+        if(this.x <= 0-this.width){
+            this.reset();
         }
+    }
+
+    reset() {
+        this.x = game.config.width;
     }
 }
