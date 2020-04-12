3a32ff75c6d4e08dca77bdaa44e45b3e56236486
Sean <kkang4@ucsc.edu>
2020-04-11 22:03:24 -0700

    Added rocket and spaceships

---
+++ src/prefabs/Spaceship.js
@@ -0,0 +1,17 @@
+// Spaceship prefab
+class Spaceship extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame, pointValue) {
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this);
+        this.points = pointValue;
+    }
+
+    update(){
+        //move spaceship left
+        this.x -= 3;
+        //wraparound screen bounds
+        if(this.x <= 0-this.width) {
+            this.x = game.config.width;
+        }
+    }
+}
